1b1a7ca732a24d7d833222e929c75fa1d275097a
Aditya Kumar <adityadav294@gmail.com>
2025-07-08 20:14:26 +0530

    Create App-simple.jsx

--- frontend/src/App-simple.jsx
+++ frontend/src/App-simple.jsx
@@ -1,1 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+      <h1>Virtual Land Registry</h1>
+      <p>The application is loading...</p>
+      <p>If you see this, React is working correctly.</p>
+    </div>
+  )
+}
+
+export default App
 
